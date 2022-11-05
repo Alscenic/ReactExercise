2c3cff716523fda3fc41d442cd2a16cc34f0d18a
Alscenic <alscenic@gmail.com>
2022-11-05 07:36:18 -0400

    Remove unneeded FontAwesome assets (oops) Add Icon component

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Icon from "./Icon";
 
 class Header extends React.Component
 {
---
+++ src/Icon.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+export default class Icon extends React.Component<{ icon?: string }, {}>
+{
+    render()
+    {
+        const icon = this.props.icon ?? "bug";
+        return (
+            <i className={"fas fa-" + icon}></i>
+        );
+    }
+}
